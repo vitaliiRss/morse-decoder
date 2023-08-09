f5cb9b314625dcbb8ee6d0e941e021c2de1890f8
Vitalii <vitalii.hryshko.rss@gmail.com>
2023-08-09 14:30:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const stringToArray = [];
+    let encryptedWord = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        stringToArray.push(expr.substring(i, i + 10));
+    }
+
+    for (const item of stringToArray) {
+        console.log(item);
+
+        let stringOf2Characters = '';
+
+        for (let j = 0; j < item.length; j = j + 2) {
+            if (item[j] === '*') {
+                stringOf2Characters = ' ';
+                break;
+            }
+
+            if (item[j] + item[j + 1] === '00') {
+                continue;
+            }
+
+            if (item[j] + item[j + 1] === '10') {
+                stringOf2Characters = stringOf2Characters + '.';
+            }
+
+            if (item[j] + item[j + 1] === '11') {
+                stringOf2Characters = stringOf2Characters + '-';
+            }
+
+            console.log(stringOf2Characters);
+        }
+
+        encryptedWord = `${encryptedWord}${stringOf2Characters !== ' ' ? MORSE_TABLE[stringOf2Characters] : ' '}`;
+    }
+
+    return encryptedWord;
 }
 
 module.exports = {
